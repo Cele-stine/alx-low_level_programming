6552e9a803aaa485b1ee60f446acd057e65db2d6
Celestine Wangechi <wangechicelestine@gmail.com>
2023-04-27 16:41:53 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,16 +3,24 @@
 /**
  * print_most_numbers - Print num except 2 and 4
  *
- * Return: void
+ * Return: The numbers from 0 up to 9
  */
 
 void print_most_numbers(void)
 {
-	char c;
+	char c = 0;
 
 	for (c = 0; c <= 9; c++)
 	{
-		if (!(c == 2 || c == 4))
+		if (c == 2 || c == 4)
+			if (x == 2 || x==4)
+			{
+				continue;
+			}
+			else 
+			{putchar ( x + 0);
+			}
+	}
 			-putchar(c);
 	}
 	_putchar("\n");

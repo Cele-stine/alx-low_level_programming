d80f943a406044b7c4efa98caa49621b380b0bdd
Celestine Wangechi <wangechicelestine@gmail.com>
2023-04-28 05:40:16 -0700

    lines

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,12 @@ void more_numbers(void)
 {
 	int x, y;
 
-	for (x = '0'; x < 10; x++)
+	for (x = '1'; x <= 10; x++)
 	{
 	for (y = '0'; y <= 14; y++)
 	{
-	if (y > '9')
-	{
-	_putchar((y / 10) + '0');
-	}
+	if (y >= 10)
+	_putchar('1');
 	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');

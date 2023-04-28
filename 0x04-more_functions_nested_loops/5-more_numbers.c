ed9d31a3474c5ef81823849e79f66ee643780eef
Celestine Wangechi <wangechicelestine@gmail.com>
2023-04-28 02:07:55 -0700

    More numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,20 @@
  */
 
 void more_numbers(void)
+
 {
 	int x, y;
-	for (x = '0'; x < '10'; x++)
+
+	for (x = '0'; x < 10; x++)
 	{
-	for (y = '0'; y <= '14'; y++)
+	for (y = '0'; y <= 14; y++)
 	{
 	if (y > '9')
 	{
-	_putchar((y / '10') + '0');
+	_putchar((y / 10) + '0');
 	}
-	_putchar((y % '10') + '0');
+	_putchar((y % 10) + '0');
 	}
 	_putchar('\n');
 	}
+}

2064ebc29fcc9087ddb469649e8b81dc59443179
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-03 15:34:57 -0700

    Dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - print tha new value of an int to 98
+ *
+ * @n: input
+ *
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0-reset_to_98
+change the value on an integer but changing the value of the pointer(derefering)

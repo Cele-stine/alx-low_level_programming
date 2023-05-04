dfbc8abd8c9fbf658369f44425e5b89cc095d667
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-04 06:07:46 -0700

    Reverse

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,23 +9,17 @@
 
 void puts2(char *str)
 {
-	int longi = 0;
-	int t = 0;
-	char *y = str;
-	int o;
+	int i;
+	int j = 0;
 
-	while (*y != '\0')
+	while (str[j] != '\0')
 	{
-		y++;
-		longi++;
+		j++;
 	}
-	t = longi - 1;
-	for (o = 0; o <= t; o++)
+
+	for (i = 0; i < j; i += 2)
 	{
-		if (o % 2 == 0)
-		{
-			_putchar(str[0]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }

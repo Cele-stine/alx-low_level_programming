ae05785447235c12831f37c48eaf8880e31a8fe6
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-17 11:49:31 -0700

    concatenate strings

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *str_concat(char *s1, char *s2);
 /**
  * main - check the code for ALX School students.
  *
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,9 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _str_concat - a function that concantenates two strings
+ * str_concat - a function that concantenates two strings
  * with the malloc function
  * @s1: the fist srting
  * @s2: the second string
@@ -11,10 +10,10 @@
  * Return: 0 (success)
  */
 
-char *_str_concat(char *s1, char *s2)
+char *str_concat(char *s1, char *s2)
 {
 	int i, j;
-	char *result;
+	char *concat;
 
 	if (s1 == NULL || s2 == NULL)
 	{
@@ -26,26 +25,26 @@ char *_str_concat(char *s1, char *s2)
 		i++;
 	while (s2[j] != '\0')
 		j++;
-	result = malloc(sizeof(char) * (i + j + 1));
+	concat = malloc(sizeof(char) * (i + j + 1));
 
-	if (result == NULL)
+	if (concat == NULL)
 	{
 		return (NULL);
 	}
 	i = j = 0;
 	while (s1[i] != '\0')
 	{
-		result[i] = s1[i];
+		concat[i] = s1[i];
 		i++;
 	}
 	while (s2[j] != '\0')
 	{
-		result[i] = s2[j];
+		concat[i] = s2[j];
 		i++;
 		j++;
 	}
 
-	result[i] = '\0';
-	return (result);
+	concat[i] = '\0';
+	return (concat);
 
 }

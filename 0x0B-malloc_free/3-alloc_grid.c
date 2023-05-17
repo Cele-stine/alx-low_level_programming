05fd4f99dadad6906df14cbd84ed870d75401100
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-17 16:17:14 -0700

    two dimension arrays

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - a function that retuns a pointer
+ * of a two dimension array
+ * @width: rows of the array
+ * @height: column in the array
+ *
+ * Return: grid
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	int **grid = (int **)malloc(width * sizeof(int *));
+
+	if (grid == NULL)
+		return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		grid[i] = (int *)malloc(height * sizeof(int));
+
+		for (j = 0; j < height; j++)
+		{
+			grid[i][j] =  0;
+		}
+	}
+	return (grid);
+}

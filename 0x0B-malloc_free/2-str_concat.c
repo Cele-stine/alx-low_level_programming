d267d692329ab4588fac6e4264911334597b5228
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-17 11:59:18 -0700

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,11 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *concat;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+	{
+		return (" ");
+	}
+	if (s2 == NULL)
 	{
 		return (" ");
 	}

231bf6bf8fe9ddb4162e6c6dce9333635f804910
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-26 14:10:48 -0700

    Functoin that executes a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - prints each array element on a newline
+ * @array: an array
+ * @size: amount of elements to print
+ * @action: pointer to print in regular or hex
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int a;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (a = 0; a < size; a++)
+	{
+		action(array[a]);
+	}
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -2,3 +2,4 @@ FUNCTION POINTERS
 
 0.print_name
 A function that prints a name
+1.a function that executes a function given as a parameteron each element of an array

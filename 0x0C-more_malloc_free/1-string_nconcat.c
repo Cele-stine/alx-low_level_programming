6310fffb4a9d08b19f884b9dc1dfbe02efa504ea
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-30 02:22:00 -0700

    concantenate string

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,43 +7,42 @@
  * @s1: The first string
  * @s2: The second string
  * @n: the parameter to determine the length of s2
- * Return: pointer to a concanteted string
+ * Return: pointer to  concantenate two strings
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0;
-	unsigned int len2 = 0;
-	unsigned int i = 0;
-	unsigned int j = 0;
-	char *str;
+	char *s;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
 	while (s1 && s1[len1])
 		len1++;
+
 	while (s2 && s2[len2])
 		len2++;
 
 	if (n < len2)
-		str = malloc(sizeof(char) * (len1 + n + 1));
-	else
-		str = malloc(sizeof(char) * (len1 + len2 + 1));
+		s = malloc(sizeof(char) * (len1 + n + 1));
 
-	if (str == NULL)
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (!s)
 		return (NULL);
 
 	while (i < len1)
 	{
-		str[i] = s1[i];
-	}
-	while (n < len2 && i < (len1 + n))
-	{
-		str[i++] = s2[j++];
-	}
-	while (n >= len2 && i < (len1 + len2))
-	{
-		str[i++] = s2[j++];
+		s[i] = s1[i];
+		i++;
 	}
 
-	str[i] = '\0';
-	return (str);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+
+	s[i] = '\0';
+
+	return (s);
 }

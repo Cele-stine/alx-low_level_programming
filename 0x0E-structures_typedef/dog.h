64c1505f4c3f786aec5ae3ad9ab70b3cb36cc126
Celestine Wangechi <wangechicelestine@gmail.com>
2023-05-30 01:17:39 -0700

    New dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - discription of dog
  * @name: First member
@@ -16,16 +15,14 @@ struct dog
 	float age;
 	char *owner;
 };
-
 /**
  * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
+char *strcpy(char *dest, char *src);
+int _strlen(char *s)
 #endif

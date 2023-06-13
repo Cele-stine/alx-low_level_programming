cd7d674684d7ddfc6c8385f7a8cd8b2f3dd4271f
Celestine Wangechi <wangechicelestine.gmail.com>
2023-06-13 06:44:23 -0700

    The woman who has no imagination thas no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - a function that retuns a new string which is a pointer to string
+ * str, memory of the new string can be obtained by malloc and freed by free
+ * @str: the string to duplicate
+ *
+ * Return: pointer to new sring
+ */
+
+char *_strdup(char *str)
+{
+	char *dup;
+	int length;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	length = strlen(str);
+
+	dup = malloc(sizeof(char) * (length + 1));
+
+	if (dup == NULL)
+	{
+		return (NULL);
+	}
+
+	else
+	{
+		strcpy (dup, str);
+	}
+	return (dup);
+}
+
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,5 @@ MALLOC AND FREE
 0-create_array
 this function creates an array of chars and initializes it with a specific char using malloc
 it returns a pointer to thr array of char
+1-strdup
+here get the length of the string str copy the string then use malloc to return a pointer to the copy of the string str 

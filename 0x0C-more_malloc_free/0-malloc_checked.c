56170290556a1d0d590e3ef9c09dd0fdec8078b2
Celestine Wangechi <wangechicelestine.gmail.com>
2023-06-15 04:14:38 -0700

    Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: the int to allocate
+ *
+ * Return: Nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,4 @@
+MORE MALLOC AND FREE
+
+0-malloc_checked
+this function that allocate memory using malloc and exists with 98 if the malloc fails

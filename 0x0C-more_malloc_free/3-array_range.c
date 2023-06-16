8a6672291cbdfbbd1436042b5bd8659afeda18f5
Celestine Wangechi <wangechicelestine.gmail.com>
2023-06-16 01:35:52 -0700

    allocate the range of an array

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array-range - creates an array of ints
+ * @min: the minimum range of values stored
+ * @max: maximum range of values stored
+ *
+ * Return: pointer to new array
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, size;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+
+	size = max - min + 1;
+
+	ptr = malloc(sizeof(int) * size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; min <= max; i++)
+	{
+		ptr[i] = min++;
+	}
+	return (ptr);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -4,3 +4,7 @@ MORE MALLOC AND FREE
 this function that allocate memory using malloc and exists with 98 if the malloc fails
 1-string_nconcat
 this function that concantenates two strings returning a dynamically allocated string that reps the concantenation s2 being where n chars are appended from it
+2-calloc
+a function that allocates an array using malloc
+3-array_range
+a function that allocates a range of a array from maximum to minimum dynamically
